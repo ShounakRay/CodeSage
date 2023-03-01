d46126afb3216bfbd83ba21597971dc46585b251
Joseph Tey <jjttey@gmail.com>
2023-02-28 17:31:41 -0800

    getting the glue ready

--- the_glue.py
+++ the_glue.py
@@ -1,1 +1,10 @@
-# This is what will call information from all the modules
+from Modules.Code2Explanation.code2doc import Code2DocModule
+from Modules.Code2Code.Extracontent.code_snippet_dataset import CodeSnippetDataset
+
+dataset = CodeSnippetDataset(languages=["Python"])
+code_snippets = dataset.get_n_snippets(10)
+
+code2doc = Code2DocModule(code_snippets)
+data_with_docs = code2doc.get_docs()
+
+print(data_with_docs)
