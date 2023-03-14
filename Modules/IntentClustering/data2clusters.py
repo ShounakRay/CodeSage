f8262f5115f997664ab4f2d58299f32e3419f25f
Shounak Ray <shounak@stanford.edu>
2023-03-14 00:55:24 -0700

    FInalized all clustering stuff

--- Modules/IntentClustering/data2clusters.py
+++ Modules/IntentClustering/data2clusters.py
@@ -1,10 +1,14 @@
 from sklearn.cluster import KMeans, DBSCAN
 import numpy as np
-from Modules.IntentClustering.vectorizer import _get_data, vectorize
+from Modules.IntentClustering.vectorizer import vectorize
 import json
+import _pickle as cPickle
 import numpy as np
 
 from datasets import load_dataset
+from multiprocessing import Pool
+# from Modules.IntentClustering.multiprocessed import MULT_preprocess
+import os
 
 """
 FOR TESTING PURPOSES:
@@ -12,30 +16,46 @@ git clone https://github.com/TheAlgorithms/Python/tree/master
 Each folder is it's own intent category (roughly)
 Just feed these through the clustering algorithm(s)
 See how many clusters it comes up with
-
 """
 
 class IntentClustering():
-   def __init__(self, function_ids, code_reference, IC_ALGO, IC_KVAL):
+   def __init__(self, function_ids, code_reference):
       self.function_ids = function_ids
       self.code_reference = code_reference
-      self.IC_ALGO = IC_ALGO
-      self.IC_KVAL = IC_KVAL
-      self.data, self.v_data, self.doc_to_id = self._preprocess()
 
    def write_to_file(self, data):
       with open('clusters.json', 'w') as fp:
         json.dump(data, fp)
 
-   def _preprocess(self, embedder="STrans"):
+   def _preprocess(self, embedder="STrans", load_numpys=True, save_numpys=False):
       # We're skipping this part for local testing
+      # with Pool(os.cpu_count() - 1) as pool:
+      #    mapping = pool.starmap(MULT_preprocess, list(self.code_reference.items()))
+      # doc_to_id = dict(mapping)
+
+      if load_numpys:
+         with open('Modules/IntentClustering/storeData/data.pickle', 'rb') as fp:
+            data = cPickle.load(fp)
+         with open('Modules/IntentClustering/storeData/v_data.pickle', 'rb') as fp:
+            v_data = cPickle.load(fp)
+         with open('Modules/IntentClustering/storeData/doc_to_id.pickle', 'rb') as fp:
+            doc_to_id = cPickle.load(fp)
+
       doc_to_id = {}
       for code_id, info in self.code_reference.items():
          documentation_i = info["documentation"]
          doc_to_id[documentation_i] = code_id
-      data = list(doc_to_id.values())
-      # data = [metaData["documentation"] for metaData in list(self.code_reference.values())]
+
+      data = list(doc_to_id.keys())
       v_data = vectorize(data=data, embedder=embedder)
+
+      if save_numpys:
+         with open('Modules/IntentClustering/storeData/data.pickle', 'wb') as fp:
+            cPickle.dump(data, fp) # , protocol=pickle.HIGHEST_PROTOCOL)
+         with open('Modules/IntentClustering/storeData/v_data.pickle', 'wb') as fp:
+            cPickle.dump(v_data, fp) # , protocol=pickle.HIGHEST_PROTOCOL)
+         with open('Modules/IntentClustering/storeData/doc_to_id.pickle', 'wb') as fp:
+            cPickle.dump(doc_to_id, fp) # , protocol=cPickle.HIGHEST_PROTOCOL)
 
       return data, v_data, doc_to_id
 
@@ -45,20 +65,21 @@ class IntentClustering():
          cluster_id = self._labels[index]
          documentation_i = self.data[index]
          data_app = self.doc_to_id[documentation_i]
+         """
+         Exmaple format of `doc_to_id.`
+         {'Sends Geolocation data to falcon REST API': 'fd798e8f-157a-4214-9ddd-5cbfe607fa51'}
+         """
+         # raise ValueError
          # Next line is for testing only
          # data_app = documentation_i
          if cluster_id not in clusters.keys():
             clusters[cluster_id] = [data_app]
          else:
             clusters[cluster_id].append(data_app)
-
-      self.write_to_file(clusters)
    
       return clusters
 
-   def kmeans(self, n_clusters=10):
-      # n_clusters = 4
-      # data = _get_data(source = "json")
+   def kmeans(self, n_clusters=10, n_jobs=-1):
       kmeans = KMeans(n_clusters=n_clusters, random_state=41, n_init="auto").fit(self.v_data)
       self._labels = kmeans.labels_ = np.array(kmeans.labels_, dtype=int)
 
@@ -66,24 +87,28 @@ class IntentClustering():
       dbscan = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=n_jobs).fit(self.v_data)
       self._labels = dbscan.labels_ = np.array(dbscan.labels_, dtype=int)
    
-   def core_get_clusters(self, embedder="STrans", method='kmeans', n_clusters=10, eps=0.5, min_samples=5, n_jobs=-1):
-      assert embedder in ("tfidf", "STrans", "Elmo")
+   def core_get_clusters(self, embedder="strans", method='kmeans', n_clusters=10, eps=0.5, min_samples=5, n_jobs=-1):
+      method, embedder = method.lower(), embedder.lower()
+      
+      assert embedder in ("tfidf", "strans", "elmo")
       assert method in ("kmeans", "dbscan")
-      assert type(n_clusters) == np.number
-      assert (type(eps) == np.number) and (0.001 <= eps <= 1.0)
+      assert type(n_clusters) == int
+      assert 0.001 <= eps <= 1.0
       assert n_jobs in (None, -1)
       assert type(min_samples) == int
       
-      self._preprocess(embedder=embedder)
+      print("Clustering\tPre-processing...")
+      self.data, self.v_data, self.doc_to_id = self._preprocess(embedder=embedder, save_numpys=True, load_numpys=False)
       
-      method = method.lower()
       if method == 'kmeans':
+         print("Clustering\tRunning KMeans...")
          self.kmeans(n_clusters=n_clusters, n_jobs=n_jobs)
       elif method == 'dbscan':
+         print("Clustering\tRunning DBSCAN...")
          self.dbscan(eps=eps, min_samples=min_samples, n_jobs=n_jobs)
       else:
          return ValueError("No other methods implemented yet.")
-
+      print("Clustering\tPost-processing...")
       clusters = self._postprocess()
 
       # This is the format mapping cluster_ids to list of function_ids in a dictionary
